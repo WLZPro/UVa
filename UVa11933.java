74e57f0b3013d648855bd0f171bb17f4815f86db
WLZ <wowo.luo.zhiyi@gmail.com>
2017-12-15 11:33:24 +0100

    Bit operationsgit add .

---
+++ UVa11933.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+		Main main = new Main();
+		main.begin();
+    }
+    void begin() {
+		Scanner inputScanner = new Scanner(System.in);
+		while (true) {
+			int n = inputScanner.nextInt();
+			if (n == 0) {
+				break;
+			}
+			int out1 = 0, out2 = 0;
+			boolean a = true;
+			while (n > 0) {
+				int t = (n & (-n));
+				if (a) {
+					out1 += t;
+				} else {
+					out2 += t;
+				}
+				n -= t;
+				a = !a;
+			}
+			System.out.println(out1 + " " + out2);	
+		}
+		inputScanner.close();
+    } 
+}
+
+
+
