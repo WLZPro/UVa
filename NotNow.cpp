ea5c8905908718e61a39be1cfc766db7e21520c8
WLZ <wowo.luo.zhiyi@gmail.com>
2018-01-28 16:42:11 +0100

    Hope this will work

--- NotNow.cpp
+++ NotNow.cpp
@@ -3,14 +3,13 @@
 #include <algorithm>
 using namespace std;
 #define OFFSET 2550
-int arr[103], n, m, full_sum, max_memo[103][5102][52], min_memo[103][5102][52];
-
+int arr[103], n, m, full_sum, max_memo[103][5102][52], min_memo[103][5102][52], min_val;
 
 
 int calcMax(int i, int sum, int takenNum) {
-    if (takenNum > min(n, m)) return -1000000000;
-    if (i == n + m && takenNum == min(n, m)) return sum * (full_sum - sum);
-    if (i == n + m && takenNum != min(n, m)) return -1000000000;
+    if (takenNum > min_val || (i == n + m && takenNum != min_val)) return -1000000000;
+    if (takenNum + n + m - i < min_val) return -1000000000; 
+    if (takenNum == min_val) return max_memo[i][sum+OFFSET][takenNum] = sum * (full_sum - sum);
     if (max_memo[i][sum + OFFSET][takenNum] != -1) return max_memo[i][sum+OFFSET][takenNum];
 
     int res = max(calcMax(i + 1, sum + arr[i], takenNum + 1), calcMax(i + 1, sum, takenNum));
@@ -18,9 +17,9 @@ int calcMax(int i, int sum, int takenNum) {
 }
 
 int calcMin(int i, int sum, int takenNum) {
-    if (takenNum > min(n, m)) return 1000000000;
-    if (i == n + m && takenNum == min(n, m)) return sum * (full_sum - sum);
-    if (i == n + m && takenNum != min(n, m)) return 1000000000;
+    if (takenNum > min_val || (i == n + m && takenNum != min_val)) return 1000000000;
+    if (takenNum + n + m - i < min_val) return 1000000000; 
+    if (takenNum == min_val) return min_memo[i][sum+OFFSET][takenNum] = sum * (full_sum - sum);
     if (min_memo[i][sum + OFFSET][takenNum] != -1) return min_memo[i][sum+OFFSET][takenNum];
 
     int res = min(calcMin(i + 1, sum + arr[i], takenNum + 1), calcMin(i + 1, sum, takenNum));
@@ -35,6 +34,7 @@ int main() {
             scanf("%d", &arr[i]);
             full_sum += arr[i];
         }
+        min_val = min(n, m);
         memset(max_memo, -1, sizeof max_memo);
         memset(min_memo, -1, sizeof min_memo);
         printf("%d %d\n",calcMax(0, 0, 0) ,calcMin(0, 0, 0));
