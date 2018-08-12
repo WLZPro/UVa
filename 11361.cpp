75a9802f7b8cec09a75b9aba382943e1d3e26661
WLZPro <34100839+WLZPro@users.noreply.github.com>
2018-08-12 20:56:04 +0200

    Optimize solution with precomputation

--- 11361.cpp
+++ 11361.cpp
@@ -1,54 +1,60 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#include <array>
 #include <cmath>
-#include <algorithm>
 
 using namespace std;
 
-int calc(int a, int k) {
-  if (a == 0) {
-    return 1;
-  }
-  vector<int> ad;
-  while (a > 0) {
-    ad.push_back(a % 10);
-    a /= 10;
-  }
-  //reverse(ad.begin(), ad.end());
-  int len = (int) ad.size();
-  vector< vector< vector<int> > > dp(len + 1, vector< vector<int> >(min(k, 101), vector<int>(k, 0)));
+const int MAX_LOG = 11;
+
+vector< vector< vector<int> > > dp;
+vector<int> pw;
+
+void precalc(int k) {
+  dp.assign(MAX_LOG + 1, vector< vector<int> >(min(k, 101), vector<int>(k)));
   dp[0][0][0] = 1;
-  for (int i = 0; i < len; i++) {
+  pw.assign(12, 1);
+  for (int i = 1; i < 12; i++) {
+    pw[i] = (pw[i - 1] * 10) % k;
+  }
+  for (int i = 0; i < MAX_LOG; i++) {
     for (int m1 = 0; m1 < min(k, 101); m1++) {
       for (int m2 = 0; m2 < k; m2++) {
         if (dp[i][m1][m2] == 0) {
           continue;
         }
         for (int x = 0; x < 10; x++) {
-          dp[i + 1][(m1 + x) % k][(m2 + x * ((int)pow(10, i)%k)) % k] += dp[i][m1][m2];
+          dp[i + 1][(m1 + x) % k][(m2 + x * pw[i]) % k] += dp[i][m1][m2];
         }
       }
     }
   }
+}
 
+int calc(int a, int k) {
   int ans = 0;
+  vector<int> digits;
+  while (a > 0) {
+    digits.push_back(a % 10);
+    a /= 10;
+  }
+  int len = (int) digits.size();
   int prev = 0;
   int prevnum = 0;
   for (int t = len - 1; t >= 0; t--) {
-    for (int j = 0; j < ad[t]; j++) {
-      int idx1 = (k - ((prev + j) % k)) % k;
-      int idx2 = (k - ((prevnum * (int) pow(10, t + 1) + j * (int) pow(10, t)) % k)) % k;
-      if ((idx1 < (int)dp[0].size()) && idx2 < (int)dp[0][0].size()) {
-        ans += dp[t][(k - ((prev + j) % k)) % k][(k - ((prevnum * (int) pow(10, t + 1) + j * (int) pow(10, t)) % k)) % k];
+    for (int j = 0; j < digits[t]; j++) {
+      int m1 = (k - ((prev + j) % k)) % k;
+      int m2 = (k - ((prevnum * pw[t + 1] + j * pw[t])) % k) % k;
+      if (m1 >= (int) dp[0].size() || m2 >= (int) dp[0][0].size()) {
+        continue;
       }
+      ans += dp[t][m1][m2];
     }
-    
-    prev += ad[t];
-    prevnum *= 10;
-    prevnum += ad[t];
+    prev += digits[t];
+    prevnum = prevnum * 10 + digits[t];
   }
-  //cout << prevnum << endl;
-  if (prevnum % k == 0 && prev % k == 0) {
+  if (prev % k == 0 && prevnum % k == 0) {
     ans++;
   }
   return ans;
@@ -62,6 +68,7 @@ int main() {
   while (tt--) {
     int a, b, k;
     cin >> a >> b >> k;
+    precalc(k);
     cout << calc(b, k) - calc(a - 1, k) << endl;
   }
   return 0;
